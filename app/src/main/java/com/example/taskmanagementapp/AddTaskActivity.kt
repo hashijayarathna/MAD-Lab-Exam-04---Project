f9593e0876dfcd23b5ddc584a74ced9f94b56674
hashijayarathna <147933342+hashijayarathna@users.noreply.github.com>
2024-05-12 17:38:51 +0530

    project added to github

--- app/src/main/java/com/example/taskmanagementapp/AddTaskActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/AddTaskActivity.kt
@@ -7,6 +7,7 @@ import com.example.taskmanagementapp.databinding.ActivityAddTaskBinding
 
 class AddTaskActivity : AppCompatActivity() {
 
+    // Declare variables for data binding and database helper
     private lateinit var binding: ActivityAddTaskBinding
     private lateinit var db: TaskDatabaseHelper
 
@@ -15,14 +16,25 @@ class AddTaskActivity : AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize the database helper
         db = TaskDatabaseHelper(this)
 
         binding.saveButton.setOnClickListener {
+
+            // Retrieve title and content from EditText fields
             val title = binding.titleEditText.text.toString()
             val content = binding.contentEditText.text.toString()
+
+            // Create a Task object with the retrieved data
             val task = Task(0, title, content)
+
+            // Insert the task into the database
             db.insertTask(task)
+
+            // Finish the activity
             finish()
+
+            // Display a message indicating that the task is saved
             Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
         }
     }
