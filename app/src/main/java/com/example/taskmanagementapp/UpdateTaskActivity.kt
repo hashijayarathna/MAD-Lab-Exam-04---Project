2b3133928402698b9930fef2bbdab40cff8b959f
hashijayarathna <147933342+hashijayarathna@users.noreply.github.com>
2024-05-12 18:19:17 +0530

    project added to github

--- app/src/main/java/com/example/taskmanagementapp/UpdateTaskActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/UpdateTaskActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.taskmanagementapp.databinding.ActivityUpdateTaskBinding
 
+// Activity for updating a task
 class UpdateTaskActivity : AppCompatActivity() {
 
+    // Declare variables for data binding, database helper, and task ID
     private lateinit var binding: ActivityUpdateTaskBinding
     private lateinit var db: TaskDatabaseHelper
     private var taskId: Int = -1
@@ -18,22 +20,29 @@ class UpdateTaskActivity : AppCompatActivity() {
 
         db = TaskDatabaseHelper(this)
 
+        // Get the task ID passed from the previous activity
         taskId = intent.getIntExtra("task_id", -1)
+        // If task ID is invalid, finish the activity
         if (taskId == -1){
             finish()
             return
         }
 
+        // Retrieve the task details from the database and move into the EditText fields
         val task = db.getTaskByID(taskId)
         binding.updateTitleEditText.setText(task.title)
         binding.updateContentEditText.setText(task.content)
 
+        // Set click listener for the save button
         binding.updateSaveButton.setOnClickListener {
             val newTitle = binding.updateTitleEditText.text.toString()
             val newContent = binding.updateContentEditText.text.toString()
             val updatedTask = Task(taskId, newTitle, newContent)
+
+            // Update the task in the database
             db.updateTask(updatedTask)
             finish()
+            // Display a message when changes are saved
             Toast.makeText(this, "Changes Saved.", Toast.LENGTH_SHORT).show()
         }
     }
